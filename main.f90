93ab5b8941ba02b8432078c2363179965117bfff
hikalium <hikalium@hikalium.com>
2017-01-22 00:46:46 +0900

    Add interface for sigmoid

--- main.f90
+++ main.f90
@@ -2,10 +2,15 @@ program main
   use modfnn
   implicit none
 
-  print *, relu(0d0)  
-  print *, relu(4d0)  
-  print *, relu(2d0)  
-  print *, relu(-4d0)  
-  print *, relu(-2d0)  
+  real(8) :: X(1, 2) = reshape((/ 1.0, 0.5 /), (/1, 2/))
+  real(8) :: W1(2, 3) = reshape( &
+    (/ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 /), (/2, 3/))
+  real(8) :: B1(1, 3) = reshape((/ 0.1, 0.2, 0.3 /), (/1, 3/))
+
+  call disp(X) 
+  call disp(W1) 
+  call disp(B1) 
+
+  call disp(sigmoid(matmul(X, W1) + B1, size(B1, 2), size(B1, 1)))
 
 end program
--- modfnn.f90
+++ modfnn.f90
@@ -1,14 +1,61 @@
 module modfnn
   implicit none
+  interface sigmoid
+    module function sigmoidr(m) result(r)
+      real(8), intent(in) :: m
+      real(8) :: r
+    end function
 
+    module function sigmoidm(m) result(r)
+      integer :: xs, ys
+      real(8), intent(in) :: m(ys, xs)
+      real(8) :: r(ys, xs)
+    end function
+  end interface
 contains
-  function sigmoid(x) result(r)
-    double precision :: x, r
-    r = 1 / (1 + exp(-x))
-  end function
   function relu(x) result(r)
-    double precision :: x, r
+    real(8) :: x, r
     r = max(x, 0d0)
   end function
-
+  subroutine disp(mat)
+    real(8) mat(:, :)
+    integer i
+    do i = 1, size(mat, 1)
+      write(*, "(100e12.4)") mat(i, 1:)
+    end do
+  end subroutine
+  subroutine tic(t1)
+    integer, intent(inout) :: t1
+    call system_clock(t1)
+  end subroutine tic
+  subroutine toc(t1)
+    integer, intent(inout) :: t1
+    integer :: t2, t_rate, t_max, diff
+    call system_clock(t2,t_rate,t_max)
+    if (t2 < t1) then
+    diff = (t_max - t1) + t2 + 1
+    else
+    diff = t2 - t1
+    end if
+    write(*,*) "Time = ", diff/dble(t_rate)
+    t1 = diff
+  end subroutine toc
+  function sigmoidr(m) result(r)
+    real(8), intent(in) :: m
+    real(8) :: r
+    r = 1d0 / (1d0 + exp(-m))
+  end function
+  function sigmoidm(m, xs, ys) result(r)
+    integer :: xs, ys
+    real(8), intent(in) :: m(ys, xs)
+    real(8) :: r(ys, xs)
+    !
+    integer :: y, x
+    do y = 1, size(m, 1)
+      do x = 1, size(m, 2)
+        r(y, x) = sigmoidr(m(y, x))
+      end do 
+    end do    
+  end function
 end module
+
