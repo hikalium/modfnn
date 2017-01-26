1c76abb4c8ffad44cf5cf205e3d092e48cd61012
hikalium <hikalium@hikalium.com>
2017-01-27 01:11:31 +0900

    Add MNIST database loading (imgs, label)

--- main.f90
+++ main.f90
@@ -8,6 +8,8 @@ program main
   type(matptr) :: W(3)
   type(matptr) :: b(3)
   real(8), allocatable :: tmp(:, :)
+  type(matptr), allocatable :: imgs(:)
+  integer, allocatable :: labels(:)
   !
   allocate(W(1)%p(2, 3))
   W(1)%p = reshape((/ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 /), (/2, 3/))
@@ -34,7 +36,7 @@ program main
   call disp(softmax_m(Y))
   call disp(sum(softmax_m(Y)))
 
-  call read_MNIST()
+  call read_MNIST(1000, imgs, labels)
 
 contains
   function read_uint8(fp) result(v)
@@ -47,33 +49,47 @@ contains
       v = 256 + v
     end if
   end function
-  subroutine read_MNIST()
+  subroutine read_MNIST(n, imgs, labels)
     ! Get data from http://yann.lecun.com/exdb/mnist/
     ! and place files:
     !   'train-images-idx3-ubyte'
     !   'train-labels-idx1-ubyte'
     ! into:
     !   './data/'
-    integer :: fp, x, y, i
-    type(matptr) :: imgs(3)
-    fp = 10
-    open(fp, file="data/train-images-idx3-ubyte", &
+    integer, intent(in) :: n
+    integer :: fp_img, fp_label
+    integer :: x, y, i
+    type(matptr), allocatable :: imgs(:)
+    integer, allocatable :: labels(:)
+    allocate(imgs(n))
+    allocate(labels(n))
+    fp_img = 10
+    fp_label = 11
+    open(fp_img, file="data/train-images-idx3-ubyte", &
+      form="unformatted", access="stream")
+    open(fp_label, file="data/train-labels-idx1-ubyte", &
       form="unformatted", access="stream")
     ! Skip headers
     do x = 1, 16
-      call disp(read_uint8(fp))
+      call disp(read_uint8(fp_img))
+    end do
+    do x = 1, 8
+      call disp(read_uint8(fp_label))
     end do
     ! Read
-    allocate(imgs(1)%p(28, 28))
-    do i = 1, 10
+    do i = 1, n
+      allocate(imgs(i)%p(28, 28))
       do y = 1, 28
         do x = 1, 28
-          imgs(1)%p(y, x) = read_uint8(fp)
+          imgs(i)%p(y, x) = read_uint8(fp_img)
         end do
       end do
-      call disp_MNISTimg(imgs(1)%p)
+      labels(i) = read_uint8(fp_label)
+      call disp(labels(i))
+      call disp_MNISTimg(imgs(i)%p)
     end do
-    close(fp)
+    close(fp_label)
+    close(fp_img)
   end subroutine
   subroutine disp_MNISTimg(m)
     real(8) :: m(:, :)
@@ -84,8 +100,12 @@ contains
           write(*, fmt='(A2)', advance='no') " "
         else if(m(y, x) < 16) then
           write(*, fmt='(A2)', advance='no') "."
-        else 
+        else if(m(y, x) < 32) then
+          write(*, fmt='(A2)', advance='no') ","
+        else if(m(y, x) < 64) then
           write(*, fmt='(A2)', advance='no') "x"
+        else 
+          write(*, fmt='(A2)', advance='no') "#"
         end if
       end do
       write(*, fmt='()')
